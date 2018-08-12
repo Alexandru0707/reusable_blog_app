a6f95275f965c8b99fc1535a38e80969d8e17ad1
Alexandru0707 <alex_boys30@yahoo.com>
2018-08-12 14:51:54 +0100

    initial commit

---
+++ reusable_blog/models.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.utils import timezone
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+
+
+
+
+class Post(models.Model):
+    """
+    Here we'll define our Post model
+    """
+
+    # author is linked to a registered
+    # user, via the User model in the auth app.
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)  # the post won't be published
+    views = models.IntegerField(default=0)  # Record how often a post is seen
+    tag = models.CharField(max_length=30, blank=True, null=True)
+    image = models.ImageField(upload_to="images", blank=True, null=True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()  # function to save update the database
+        self.save()
+
+    def __unicode__(self):  # identify entries on blog (admin page)
+        return self.title
